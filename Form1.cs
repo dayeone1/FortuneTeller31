be1fe05dcd1f68106af18f1f402ad61348de3aaa
dayeon <dy071118@gmail.com>
2025-08-27 09:17:07 +0900

    #5 LoadResults() 매서드 구현

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,31 @@ namespace FortuneTeller31
         public Form1()
         {
             InitializeComponent();
+            LoadResults();
+        }
+
+        private void LoadResults()
+        {
+            try
+            {
+                string filename = "results.csv";
+                results = File.ReadAllLines(filename).ToList();
+            }
+            catch (FileNotFoundException ex) {
+                MessageBox.Show($"파일이 없어요, \n{ex.Message}", "알 수 없는 오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권환이 없어요, \n{ex.Message}", "알 수 없는 오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했어요, \n{ex.Message}", "알 수 없는 오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
