d6142f26f864a7bc27044657ed3e4d94226071a4
dayeon <dy071118@gmail.com>
2025-08-27 09:06:32 +0900

    #4 결과 데이를 results에 저장

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,10 @@ namespace FortuneTeller31
 {
     public partial class Form1 : Form
     {
+
+        List<string> results;
+
+
         public Form1()
         {
             InitializeComponent();
